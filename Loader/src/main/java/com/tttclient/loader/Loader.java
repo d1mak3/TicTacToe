32a187a733e52551a55c71133da01facc8b4fcf4
d1mak3ger@gmail.com <d1mak3ger@gmail.com>
2022-04-26 01:40:11 +0300

    Make loader load the Menu class

--- Loader/src/main/java/com/tttclient/loader/Loader.java
+++ Loader/src/main/java/com/tttclient/loader/Loader.java
@@ -1,9 +1,9 @@
 package com.tttclient.loader;
 
-import javax.swing.*;
+import com.tttclient.gui.Menu;
 
 public class Loader {
     public static void main(String[] args) {
-
+        Menu menu = new Menu();
     }
 }
