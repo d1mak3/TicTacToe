ad1d57b29514f15ec33920ec0b20e376724520eb
d1mak3 <d1mak3ger@gmail.com>
2022-05-30 16:30:58 +0300

    Fixed serialization bug

--- GameLogic/src/main/java/com/tttclient/gamelogic/IGame.java
+++ GameLogic/src/main/java/com/tttclient/gamelogic/IGame.java
@@ -3,6 +3,7 @@ package com.tttclient.gamelogic;
 import java.io.Serializable;
 
 public interface IGame extends Serializable {
+    long serialVersionUID = 42L;
     void place(int x, int y);
     LogicModel checkWin();
     void setWinCondition(int countOfModelsInARow);
